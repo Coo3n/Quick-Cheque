f0528eac270c4d819af8e71b121c84093cb668ec
PopovIV <71888203+PopovIV@users.noreply.github.com>
2023-03-29 15:54:10 +0300

    Reworked list in payment layout

--- app/src/main/java/com/example/quick_cheque/MainActivity.kt
+++ app/src/main/java/com/example/quick_cheque/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.quick_cheque
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.quick_cheque.databinding.ActivityMainBinding
+import com.example.quick_cheque.screens.room_cheque_fragments.PaymentChequeFragment
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
