444400b8e92a12ae96070243c5907b551fbdea23
coo3n <coo3n>
2023-04-01 16:16:31 +0300

    1) Удалил 2 style:    - style_black_dividing_line.xml    - style_orange_dividing_line.xml
      Заменил на обобщенную: style_mini_dividing_line.xml

--- app/src/main/java/com/example/quick_cheque/screens/room_cheque_fragments/ChoiceProductFragment.kt
+++ app/src/main/java/com/example/quick_cheque/screens/room_cheque_fragments/ChoiceProductFragment.kt
@@ -24,7 +24,7 @@ class ChoiceProductFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentChoiceProductBinding.inflate(inflater)
         return _binding.root
     }
