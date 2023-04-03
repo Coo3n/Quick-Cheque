efdaacc196155310fa58ca3eae5415f91437d9da
artemokky <ferrumsir@gmail.com>
2023-04-03 19:46:19 +0300

    locale

--- app/src/main/java/com/example/quick_cheque/MainActivity.kt
+++ app/src/main/java/com/example/quick_cheque/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.quick_cheque
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
@@ -10,9 +11,11 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.quick_cheque.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var btn_locale: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -32,7 +35,36 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+
+
         setContentView(binding.root)
+
+        var btn_locale_en: Button = findViewById(R.id.radio_english)
+        var btn_locale_ru: Button = findViewById(R.id.radio_russian)
+
+        btn_locale_en.setOnClickListener {
+            val locale = Locale("en") // выбираем язык
+            Locale.setDefault(locale) // устанавливаем его как дефолтный
+            val config = resources.configuration // получаем конфигурацию ресурсов
+            config.setLocale(locale) // устанавливаем локализацию в конфиг
+            resources.updateConfiguration(
+                config,
+                resources.displayMetrics
+            ) // обновляем конфигурацию ресурсов
+            recreate() // обновляем активити
+        }
+
+        btn_locale_ru.setOnClickListener {
+            val locale = Locale("en") // выбираем язык
+            Locale.setDefault(locale) // устанавливаем его как дефолтный
+            val config = resources.configuration // получаем конфигурацию ресурсов
+            config.setLocale(locale) // устанавливаем локализацию в конфиг
+            resources.updateConfiguration(
+                config,
+                resources.displayMetrics
+            ) // обновляем конфигурацию ресурсов
+            recreate() // обновляем активити
+        }
     }
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/quick_cheque/screens/FragmentSettings.kt
+++ app/src/main/java/com/example/quick_cheque/screens/FragmentSettings.kt
@@ -14,6 +14,7 @@ class FragmentSettings : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 }
